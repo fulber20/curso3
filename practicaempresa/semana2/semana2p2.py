310be51145325df17359f3d88f3829ebf91f532a
fulber20 <1527029@senati.pe>
2024-10-04 11:11:46 -0500

    segundo comentario

---
+++ practicaempresa/semana2/semana2p2.py
@@ -0,0 +1,10 @@
+import numpy as np
+#vectores
+v1= np.array([2,3]) 
+v2= np.array([1,4])
+#suma de vectores
+suma=v1+v2
+print("suma de los vectores: ",suma)
+escalar=3 
+v3=escalar * v1
+print("vector multiplicado por escalar: ",v3)
---
+++ practicaempresa/semana2/semna2operacion.py
@@ -0,0 +1,12 @@
+import numpy as np 
+v1=[2,4.6]
+v2=np.ones(3)
+array=([1.,1.,1.])
+v3=np.array([1,3,5])
+v4=np.arange(1, 8)
+v5=np.array([1,2,3,4,5,6,7])
+print(v1)
+print(v2)
+print(v3)
+print(v4)
+print(v5)
